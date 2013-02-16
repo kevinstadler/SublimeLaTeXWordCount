a828d70bf65894b59e3a4a29b08a173e3a49fc21
Kevin <lionandoil@gmail.com>
2013-02-16 04:33:18 +0000

    Initial commit - Sublime Word Count for LaTeX v1.0.0!

--- README.md
+++ README.md
@@ -1,26 +1,24 @@
-String Counter
-==============
-This package is plugins for Sublime Text 2.
-Count number of characters.
+## SublimeWordCount
 
-Usage notes
------------
-Select the "String Counter" from the "Tools".
+A SublimeText 2 word and character count plugin for LaTeX and plaintext files, based on [string_counter](https://github.com/naomichi-y/string_counter) by Naomichi Yamakita.
 
-![Tool bar](https://raw.github.com/naomichi-y/string_counter/master/images/toolbar.png)
+### Usage
 
-Result is displayed in the status bar.
-![All count](https://raw.github.com/naomichi-y/string_counter/master/images/all_count.png)
+Install SublimeWordCount automatically via Sublime Package Control: http://wbond.net/sublime_packages/package_control
 
-If a character is selected, only counts scope.
-![Select count](https://raw.github.com/naomichi-y/string_counter/master/images/select_count.png)
+Go to Tools -> Sublime Word Count to display word, character and line counts for the current selection, or for the entire file if nothing is selected.
 
-Licence
--------
-MIT Licence:
- * http://opensource.org/licenses/MIT
+The default key binding is _Control + Shift + C_
 
-Contacts
---------
-Writen by:
- * Naomichi Yamakita - yamakita@dtx.co.jp
+### LaTeX support
+
+The main purpose of this plugin was to support reliable word counts for files with LaTeX markup. The plugin ignores the preamble, abstract, formulas, captions, and supports excluding headers, footnotes and appendices. Have a look at the available config options in Sublime Text 2 -> Preferences -> Package Settings -> SublimeWordCount to customise what gets counted.
+
+### Suggestions
+
+ * Adding support for other markup languages should be pretty straightforward, just have a look at https://github.com/lionandoil/SublimeWordCount/blob/master/SublimeWordCount.py#L42 and chip in!
+ * A real-time word count displayed in the status bar a la [WordCount](https://github.com/titoBouzout/WordCount) would be nice and not too hard to implement. If anyone's actually using this plugin and would like this feature just let me know and I'll add it!
+ * SublimeText's scope_name() function could potentially be used to produce more robust stripping of LaTeX commands then the current regexp-wizardry.
+
+### License
+Released under the [MIT License](http://opensource.org/licenses/MIT)
---
+++ SublimeWordCount.py
@@ -0,0 +1,125 @@
+import sublime, sublime_plugin
+import re
+ 
+settings = sublime.load_settings("SublimeWordCount.sublime-settings")
+
+if settings.get("ignore_numbers"):
+    word = "[A-za-z-]+"
+else:
+    word = "[\w-]+"
+word = re.compile(word, re.U)
+
+def basic_wordcount(text):
+    words = 0
+    chars = 0
+    for w in word.findall(text):
+        words += 1
+        chars += len(w)
+    return words, chars, len(text)
+
+# infrastructure for custom word counters for different markup languages
+custom_wordcounters = { "Plain text" : basic_wordcount }
+
+def custom_wordcount(syntax,):
+    def wrap(func):
+        custom_wordcounters[syntax] = func
+    return wrap
+
+# custom word counters begin here!
+
+# word counting functions should take a single string argument and
+# return a triple of (words, characters, characters+spaces). ideally
+# the function will just strip off any comments and markup and then
+# call basic_wordcount() with the remaining cleared-up string
+
+latex_comment = re.compile("\n?%.*", re.MULTILINE)
+latex_lformula = re.compile(r"\$\$.*?\$\$", re.MULTILINE | re.DOTALL)
+latex_sformula = re.compile(r"\$.*?\$", re.MULTILINE)
+latex_abstract = re.compile(r"\\begin\{abstract\}.*?\\end\{abstract\}", re.MULTILINE | re.DOTALL)
+latex_header = re.compile(r"\\(part|chapter|(sub)*section|paragraph)\*?\{", re.MULTILINE)
+latex_footnote = re.compile(r"", re.MULTILINE)
+latex_command = re.compile(r"\\[A-Za-z]+((\s*\{.*?\})?(\s*\[.*?\])?(\s*\{.*?\})+)?\s*", re.MULTILINE | re.DOTALL)
+@custom_wordcount("LaTeX")
+def wordcount_latex(text):
+    latex_settings = settings.get("LaTeX")
+
+    # strip latex comments
+    text = latex_comment.sub(" ", text)
+
+    # skip the preamble (if there is one)
+    begin = text.find("\\begin{document}")
+    if begin != -1:
+        text = text[begin+16:]
+
+    if latex_settings.get("exclude_appendices"):
+        appendix = text.find("\\appendix")
+        if appendix != -1:
+            text = text[:appendix]
+
+    # strip formulas
+    text = latex_lformula.sub(" ", text)
+    text = latex_sformula.sub(" ", text)
+
+    # remove abstract
+    text = latex_abstract.sub("", text)
+
+    # get started with commands
+    # strip acceptable markup
+    for cmd in latex_settings.get("markup_commands"):
+        text = re.sub(r"\\" + cmd + r"\{", " ", text)
+
+    if not latex_settings.get("exclude_headers"):
+        # rescue section headers from beging purged
+        text = latex_header.sub(" ", text)
+
+    if not latex_settings.get("exclude_footnotes"):
+        # rescue footnotes from being purged
+        text = text.replace(r"\\footnote\{", " ")
+
+    # get rid of all commands that are still left
+    text = latex_command.sub(" ", text)
+
+    text = text.replace("~", " ")
+    text = text.replace("--", " ")
+    text = text.replace("{", " ")
+    text = text.replace("}", "")
+    text = text.replace("\\", "")
+    return basic_wordcount(text.strip())
+
+
+
+find_language = re.compile(r"([\w -]+)\.tmLanguage").search
+
+class SublimeWordCountCommand(sublime_plugin.TextCommand):
+    def description(self):
+        return "Display word count"
+
+    def run(self, edit):
+        selected = self.view.sel()
+        lines = len(self.view.lines(selected[0]))
+        scope = "selected region"
+        if len(selected) == 1 and selected[0].empty():
+            selected = [ sublime.Region(0, self.view.size()) ]
+            lines = self.view.rowcol(self.view.size())[0] + 1
+            scope = "entire file"
+
+        # find any custom word counters
+        language = find_language(self.view.settings().get("syntax")).group(1)
+        wordcount_fn = custom_wordcounters.get(language)
+        if wordcount_fn:
+            language = "Using custom word counting for " + language
+        else:
+            language = "No custom support for " + language + " syntax, treating file as plain text"
+            wordcount_fn = basic_wordcount
+
+        total_chars = 0
+        words = 0
+        chars = 0
+        for region in selected:
+            (rwords,rchars,rtotal) = wordcount_fn(self.view.substr(region))
+            words += rwords
+            chars += rchars
+            total_chars += rtotal
+
+        sublime.message_dialog("Sublime Word Count for %s\n\nWords:\t\t\t\t\t\t%d\nCharacters (no spaces):\t\t%d\nCharacters (with spaces):\t%d\nLines:\t\t\t\t\t\t%d\n\n%s" %
+            (scope, words, chars, total_chars, lines, language))
